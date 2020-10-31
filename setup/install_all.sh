7ee21a8afa86896bf31043cdf985f8db4e65a994
maho <maho@riken.jp>
2020-10-31 10:27:03 +0900

    .

--- setup/install_all.sh
+++ setup/install_all.sh
@@ -2,7 +2,6 @@
 
 source ../kagakubinran_setting.sh
 
-if [ x"0" = x"1" ]; then
 bash -x install_ng.sh          2>&1 | tee log.ng
 bash -x install_gcc.sh         2>&1 | tee log.gcc
 bash -x install_cmake.sh       2>&1 | tee log.cmake
@@ -13,8 +12,11 @@ bash -x install_python.sh      2>&1 | tee log.python
 bash -x install_pythonlibs.sh  2>&1 | tee log.pythonlibs
 bash -x install_mpc.sh         2>&1 | tee log.mpc
 bash -x install_pyscf.sh       2>&1 | tee log.pyscf
-fi
 bash -x install_eigen.sh       2>&1 | tee log.eigen
 bash -x install_openbabel.sh   2>&1 | tee log.openbabel
+bash -x install_jq.sh          2>&1 | tee log.jq
+bash -x install_gamess.sh      2>&1 | tee log.gamess
+bash -x install_xtb.sh         2>&1 | tee log.xtb
+bash -x install_crest.sh       2>&1 | tee log.crest
 
 
